bc8062bb6dca0aa82c4f0779b53501606bc33ee4
TareqFl <tareq.fleyfel@gmail.com>
2022-02-22 21:55:11 +0300

    added dependencies

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.render("home")
+})
+
+app.listen(3000, function(){
+  console.log("server is up and running")
+})
